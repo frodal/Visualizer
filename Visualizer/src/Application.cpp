5b6cfd00eafd8a4c239c353cd3368d72d76cb703
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-04-18 16:05:40 +0200

    Setup project

---
+++ Visualizer/src/Application.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// TODO: Include precompiled header!
+
+int main()
+{
+	std::cout << "First Lines of code!" << std::endl;
+	std::cout << "TODO: Write something useful." << std::endl;
+	std::cin.get();
+
+	return 0;
+}
