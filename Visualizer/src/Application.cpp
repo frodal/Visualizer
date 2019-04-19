ec54b46296a7e935af69753c597d99db9dc63ec7
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-04-19 16:48:33 +0200

    Update using modern OpenGL

--- Visualizer/src/Application.cpp
+++ Visualizer/src/Application.cpp
@@ -1,5 +1,6 @@
 /*
 This is the main .cpp file for the Visulazier application using OpenGL to render stuff.
+See the documentation of OpenGL, e.g., http://docs.gl/
 */
 #include "PreCompiledHeader.h" // Includes precompiled header
 
@@ -43,18 +44,30 @@ int main(void)
 	std::cout << "Current GLEW version:   " << glewGetString(GLEW_VERSION) << std::endl;
 	std::cout << "Current OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
+	/* Vertex positions*/
+	float positions[6] = {
+		-0.5f, -0.5f,
+		-0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	/* Allocates and sets an array buffer for the vertex data*/
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		/*Drawing a triangle using Legacy openGL*/
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(-0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		glEnd();
+		/*Drawing using openGL*/
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
