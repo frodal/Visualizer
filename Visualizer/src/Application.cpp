7d7ee26b2c9f05a99ba9e67da57bb2f3191bf322
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-04-20 19:30:24 +0200

    Added Basic vertex and fragment shaders

--- Visualizer/src/Application.cpp
+++ Visualizer/src/Application.cpp
@@ -5,7 +5,83 @@ See the documentation of OpenGL, e.g., http://docs.gl/
 #include "PreCompiledHeader.h" // Includes precompiled header
 
 
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
 
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::fstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (std::getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else if (type != ShaderType::NONE)
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+	return { ss[(int)ShaderType::VERTEX].str(), ss[(int)ShaderType::FRAGMENT].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)_malloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		_freea(message);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+	
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 int main(void)
 {
@@ -60,6 +136,10 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
+	ShaderProgramSource source = ParseShader("resources/shaders/Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	glUseProgram(shader);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -75,6 +155,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
--- Visualizer/src/PreCompiledHeader.h
+++ Visualizer/src/PreCompiledHeader.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#include <iostream>
 #include <GL/glew.h> // See http://glew.sourceforge.net/ for the documentation and to download the newest version!
 #include <GLFW/glfw3.h> // See https://www.glfw.org/ for the documentation and to download the newest version!
+
+#include <iostream> // Standard I/O library for C++
+#include <fstream> // Used for reading files such as the vertex and fragment shader source
+#include <string> // Standard string library for C++
+#include <sstream> // Standard String stream library for C++
+
 
