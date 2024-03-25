d0e55e90cd382d209b9fe2d2fff022be694b44e8
frodal <bfrodal@gmail.com>
2024-03-25 17:51:42 +0100

    Update Texture class for Model and Mesh classes

--- Visualizer/src/Texture.h
+++ Visualizer/src/Texture.h
@@ -5,6 +5,7 @@ class Texture
 public:
 	Texture();
 	Texture(const std::string& path);
+	Texture(const std::string& path, const std::string& textureType);
 	Texture(unsigned char* buffer, int Width, int Height);
 	~Texture();
 
@@ -15,11 +16,16 @@ public:
 
 	inline int GetWidth() const { return width; }
 	inline int GetHeigth() const { return height; }
+
+	inline const std::string& GetFilePath() const { return filepath; }
+	inline const std::string& GetTextureType() const { return textureType; }
 private:
 	void Setup(unsigned char* buffer, int Width, int Height, GLint sampling);
+	void LoadAndSetup(const std::string& path, GLint sampling);
 private:
 	unsigned int rendererID;
 	std::string filepath;
+	std::string textureType;
 	unsigned char* localBuffer;
 	int width, height, BPP;
 };
