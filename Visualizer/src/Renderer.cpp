098846059a8fa46c0c385585bf66c5985e4f77d9
frodal <bfrodal@gmail.com>
2024-03-25 17:52:22 +0100

    Update renderer to being able to render 3D models and meshes

--- Visualizer/src/Renderer.cpp
+++ Visualizer/src/Renderer.cpp
@@ -30,6 +30,42 @@ void Renderer::EnableDepth() const
 	glDepthFunc(GL_LESS);
 }
 
+void Renderer::Draw(const Model& model, Shader& shader) const
+{
+	const std::vector<Mesh>& meshes = model.GetMeshes();
+	for (unsigned int i = 0; i < meshes.size(); i++)
+	{
+		Draw(meshes[i], shader);
+	}
+}
+
+void Renderer::Draw(const Mesh& mesh, Shader& shader) const
+{
+	shader.Bind();
+
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	unsigned int normalNr = 1;
+	for (unsigned int i = 0; i < mesh.GetTextures().size(); i++)
+	{
+		mesh.GetTexture(i)->Bind(i); // activate proper texture unit before binding
+		// retrieve texture number (the N in diffuse_textureN)
+		std::string number;
+		std::string name = mesh.GetTexture(i)->GetTextureType();
+		if (name == "texture_diffuse")
+			number = std::to_string(diffuseNr++);
+		else if (name == "texture_specular")
+			number = std::to_string(specularNr++);
+		else if (name == "texture_normal")
+			number = std::to_string(normalNr++);
+
+		shader.SetUniform1i(("u_" + name + number).c_str(), i);
+		shader.SetUniform1i(("u_flag_" + name + number).c_str(), 1);
+	}
+
+	Draw(*mesh.GetVertexArray(), *mesh.GetIndexBuffer(), shader);
+}
+
 void Renderer::Draw(const VertexArray& vertexArray, const IndexBuffer& indexBuffer, const Shader& shader) const
 {
 	Draw(vertexArray, indexBuffer, shader, indexBuffer.GetCount());
@@ -57,4 +93,18 @@ void Renderer::DrawWireFrame(const VertexArray& vertexArray, const IndexBuffer& 
 	GLCall(glPolygonMode(GL_FRONT_AND_BACK, GL_LINE));
 
 	GLCall(glDrawElements(GL_TRIANGLES, indexBuffer.GetCount(), GL_UNSIGNED_INT, nullptr));
+}
+
+void Renderer::DrawWireFrame(const Model& model, const Shader& shader) const
+{
+	const std::vector<Mesh>& meshes = model.GetMeshes();
+	for (unsigned int i = 0; i < meshes.size(); i++)
+	{
+		DrawWireFrame(meshes[i], shader);
+	}
+}
+
+void Renderer::DrawWireFrame(const Mesh& mesh, const Shader& shader) const
+{
+	DrawWireFrame(*mesh.GetVertexArray(), *mesh.GetIndexBuffer(), shader);
 }
